9235f934baf35e0e5565c995e94b1782a5c7bfee
ferhatinx <82982543+ferhatinx@users.noreply.github.com>
2022-01-25 11:25:59 +0300

    if-else

---
+++ if-else-switch-case/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        int time = DateTime.Now.Month;
+        
+        if (time < 4)
+
+        {
+            System.Console.WriteLine("ilk yarı");    
+        }
+        else if(time < 8)
+        {
+            System.Console.WriteLine("ikinci yarı");
+        }
+        else
+        {
+            System.Console.WriteLine("üçüncü yarı");
+        }
+        }
+    }
+}
