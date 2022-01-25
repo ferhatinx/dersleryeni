64757ddbc7fb6d181804e4e298d7926afeede01e
ferhatinx <82982543+ferhatinx@users.noreply.github.com>
2022-01-25 11:32:15 +0300

    switch-case

--- if-else-switch-case/Program.cs
+++ if-else-switch-case/Program.cs
@@ -21,6 +21,25 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
             System.Console.WriteLine("üçüncü yarı");
         }
+        switch (time)
+        {
+            case 1:System.Console.WriteLine(time+".ay");break;
+            case 2:System.Console.WriteLine(time+".ay");break;
+            case 3:System.Console.WriteLine(time+".ay");break;
+            case 4:System.Console.WriteLine(time+".ay");break;
+            case 5:System.Console.WriteLine(time+".ay");break;
+            case 6:System.Console.WriteLine(time+".ay");break;
+            case 7:System.Console.WriteLine(time+".ay");break;
+            case 8:System.Console.WriteLine(time+".ay");break;
+            case 9:System.Console.WriteLine(time+".ay");break;
+            case 10:System.Console.WriteLine(time+".ay");break;
+            case 11:System.Console.WriteLine(time+".ay");break;
+            case 12:System.Console.WriteLine(time+".ay");break;
+            default:break;
+        }
+        
+        
+        
         }
     }
 }
