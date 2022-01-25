64b8e64460f54c3e2aa8e76349a9ef683ef5ed1f
ferhatinx <82982543+ferhatinx@users.noreply.github.com>
2022-01-25 12:03:27 +0300

    for

---
+++ for-while/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Console.WriteLine("bir sayı giriniz: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+               if (i % 2 != 0)
+               {
+                  System.Console.WriteLine(i);
+               }
+               else
+               {
+                   continue;*/
+               
+               int n = Convert.ToInt32(Console.ReadLine());
+                for(int i = 0; i<=20;i++){
+                     Console.WriteLine("n"+"x"+"i"+"="+n*i);
+            
+                }
+               }
+            }
+        }
+    }
+}
