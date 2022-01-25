d7852d9e39d8e9d84f5e562904ad722c68454ef2
ferhatinx <82982543+ferhatinx@users.noreply.github.com>
2022-01-25 13:17:42 +0300

    ARRAY

---
+++ arraysınıflar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi ={12,5,6,1,74};
+            Array.Sort(dizi);
+
+            System.Console.WriteLine(dizi) ;
+
+
+            Array.Clear(dizi,3,1);
+
+            System.Console.WriteLine(dizi);
+            Array.Reverse(dizi);
+
+            System.Console.WriteLine(dizi);
+            Array.IndexOf(dizi,5);
+
+            System.Console.WriteLine(dizi);
+        }
+    }
+}
