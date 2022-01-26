ffee3706ddab5ed9aaf5ee13f67d23d9a7f85ee8
ferhatinx <82982543+ferhatinx@users.noreply.github.com>
2022-01-26 21:05:37 +0300

    metotlar

---
+++ metotlar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+        {
+           
+      
+           int sonuc = topla(5,4);
+           System.Console.WriteLine(sonuc);
+
+          metotlar ornek =new metotlar();
+          ornek.ekranayazdır(sonuc.ToString()); 
+        }
+         static int topla(int x, int y){
+           return x+y;
+        }
+    }
+
+class metotlar
+{
+
+    public void ekranayazdır(string veri){
+        System.Console.WriteLine(veri);
+    }
+}
+}
