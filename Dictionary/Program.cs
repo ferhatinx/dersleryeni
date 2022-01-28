9e48cc5baf2798a4cd9e52d6ccacc6507eaa1f30
ferhatinx <82982543+ferhatinx@users.noreply.github.com>
2022-01-28 20:47:12 +0300

    dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> sozluk = new Dictionary<int, string>();
+
+            sozluk.Add(1,"ferhat");
+            sozluk.Add(2,"ferhat");
+            sozluk.Add(3,"ferhat");
+
+            System.Console.WriteLine(sozluk[1]);
+
+            System.Console.WriteLine(sozluk.Count());
+
+            sozluk.Remove(1);
+            foreach (var item in sozluk)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            foreach (var item in sozluk)
+            {
+                System.Console.WriteLine(sozluk.Keys);
+            }
+
+
+
+        }
+    }
+}
