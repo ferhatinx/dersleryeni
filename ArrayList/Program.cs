452821a32b2166de17cfa4e3a820eedb82db54ca
ferhatinx <82982543+ferhatinx@users.noreply.github.com>
2022-01-28 20:26:10 +0300

    arraylist

---
+++ ArrayList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         ArrayList list = new ArrayList();
+         list.Add("ayşe");
+         list.Add(4);
+         list.Add('A');
+         list.Add(true);
+
+        foreach (var item in list)
+        {
+            System.Console.WriteLine(item);
+        }
+        string[] renkler = {"kırmızı", "yeşil"};
+        List<int> sayilar = new List<int>(){1,2,3,45,6};
+        list.AddRange(renkler);
+
+
+
+
+
+
+
+        }
+    }
+}
