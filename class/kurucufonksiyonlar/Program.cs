982d740dc043223279b66fd174619a50626cba67
ferhatinx <82982543+ferhatinx@users.noreply.github.com>
2022-01-30 16:15:54 +0300

    construcktor class

---
+++ class/kurucufonksiyonlar/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calisanlar calisan1 = new calisanlar("ferhat",23);
+            
+            calisan1.calisanbilgileri();
+
+
+        }
+    }
+    class calisanlar
+    {
+        public string ad;
+        public int no;
+
+        public calisanlar(string Ad, int No)
+        {
+            this.ad = Ad;
+            this.no = No;
+        }
+        public calisanlar(){
+
+        }
+        public void calisanbilgileri(){
+
+            System.Console.WriteLine("calisan adı: {0}",ad);
+            System.Console.WriteLine("calisan no {0}",no);
+        }
+
+    }
+}
