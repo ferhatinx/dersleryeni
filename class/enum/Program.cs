5ab30b89352ff4af9c565f4f46294bbcd9f3da34
ferhatinx <82982543+ferhatinx@users.noreply.github.com>
2022-02-01 17:54:25 +0300

    enum

---
+++ class/enum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sicaklik = 23;
+
+            if (sicaklik < (int)havadurumu.normal)
+            {
+                System.Console.WriteLine("soğuk");
+            }
+            else if (sicaklik < (int)havadurumu.sicak)
+            {
+                System.Console.WriteLine("normal");
+            }
+            else
+            {
+                System.Console.WriteLine("sıcak");
+            }
+        }
+    }
+  enum Gunler 
+    {
+        Pazartesi, 
+        Sali, 
+        Carsamba, 
+        Persembe, 
+        Cuma, 
+        Cumartesi, 
+        Pazar
+    }
+    enum havadurumu
+    {
+        soguk = 5,
+        normal = 20,
+        sicak=40,
+    }
+}
