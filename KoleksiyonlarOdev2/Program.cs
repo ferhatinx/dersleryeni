94a714def71af74bed8eadce76066188027dbdf7
ferhatinx <82982543+ferhatinx@users.noreply.github.com>
2022-02-01 19:09:23 +0300

    newodev

---
+++ KoleksiyonlarOdev2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+       {
+            
+            ArrayList dizi = new ArrayList();
+            ArrayList dizi2 = new ArrayList();
+            ArrayList dizi3 = new ArrayList();
+            for (int i = 0; i <= 6; i++)
+            {
+                Console.Write("Bir sayı giriniz: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                dizi.Add(a);
+            }
+            
+            dizi.Sort();
+            
+            int toplam1=0;
+            int toplam2=0;
+            toplam1+= dizi2.Add(dizi[0]);
+            toplam1+=dizi2.Add(dizi[1]);
+            toplam1+=dizi2.Add(dizi[2]);
+              
+            toplam2+= dizi3.Add(dizi.Count-1);
+            toplam2+=dizi3.Add(dizi.Count-2);
+            toplam2+=dizi3.Add(dizi.Count-3);
+
+            toplam1 = toplam1 /3;
+            toplam2 = toplam2 /3;
+
+            System.Console.WriteLine(toplam1);
+            System.Console.WriteLine(toplam2);
+        }
+    }
+    
+}
---
+++ KoleksiyonlarOdev3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string metin = "Merhaba Dünya, bu örnek bir program";
+            string sesli = "aeıioöuü";
+ 
+            int sayac =0;
+            for (int i = 0; i < metin.Length; i++)
+            {
+                //karakter katarkter kontrol ediyoruz.
+                if(sesli.Contains(metin[i]))
+                {
+                    sayac++;
+                }
+            }
+            Console.WriteLine(metin);
+           
+ 
+        }
+    }
+}
