3f4274aebcbad97f4f960e8dd2d782e896ad116e
ferhatinx <82982543+ferhatinx@users.noreply.github.com>
2022-02-01 18:35:42 +0300

    odev

---
+++ KoleksiyonlarOdev/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+       {
+            void diziyazdirma(ArrayList a)
+            {
+            
+                foreach (var item in a)
+                {
+                    
+                    System.Console.WriteLine(item);
+                }
+
+            } 
+            ArrayList dizi = new ArrayList();
+            ArrayList dizi2 = new ArrayList();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("bir sayı giriniz: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                if (a%2==0 || a%3==0)
+                {
+                   dizi.Add(a); 
+                }
+                else if( a == 2)
+                {
+                   dizi2.Add(a);
+                }
+                else
+                {
+                   dizi2.Add(a);
+                }
+
+            }
+            System.Console.WriteLine("******AsalOlmayan*******");
+            diziyazdirma(dizi);
+            System.Console.WriteLine("******asal*******");
+            diziyazdirma(dizi2);
+
+
+        }
+    }
+    
+}
